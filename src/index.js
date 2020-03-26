9234d9d79f68c9c695789485768913f38a26a060
Dylan <dylan@dylandavenport.com>
2020-03-26 13:34:47 -0400

    Added animations and built footer.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const url =
   'https://randomuser.me/api/?inc=picture,name,email,phone,location&results=24';
 
+// Take api response and map results into an array
 axios.get(url).then(response => {
   const data = response.data.results;
   var userData = data.map(user => {
+    // Return values as markup
     return `
     <div class="user-wrapper">
     <div class="user-card" data-aos="fade-up" data-aos-offset="200" data-aos-delay="50" data-aos-anchor-placement="top-bottom">
@@ -22,31 +24,19 @@ axios.get(url).then(response => {
   document.getElementById('card-row').innerHTML = userData.join('');
 });
 
-const handleOpen = () => {
-  document.getElementById('mobile-menu').style.width = '100%';
-};
+// Function to handle mobile nav open
+const handleOpen = document
+  .getElementById('hamburger')
+  .addEventListener('click', () => {
+    document.getElementById('mobile-menu').style.width = '100%';
+  });
 
-const handleClose = () => {
-  document.getElementById('mobile-menu').style.width = '0';
-};
+// Function to handle mobile nav close
+const handleClose = document
+  .getElementById('close-btn')
+  .addEventListener('click', () => {
+    document.getElementById('mobile-menu').style.width = '0';
+  });
 
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 20, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
-});
+// Initialize scroll animations
+AOS.init({});
